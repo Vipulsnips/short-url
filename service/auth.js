98d5fe665eb396dcc3646e1d6ff1ac156721187d
Vipulsnips <vipulrawat137@gmail.com>
2026-05-06 02:05:56 +0530

    jwt authentication

--- service/auth.js
+++ service/auth.js
@@ -1,9 +1,18 @@
-const sessionIdToUserMap = new Map();
-function setUser(user,sessionId){
-  sessionIdToUserMap.set(sessionId,user);
+const jwt = require('jsonwebtoken');
+const secret="Token verified";
+function setUser(user){
+  return jwt.sign({
+    _id:user._id,
+    email:user.email,
+  },secret);
 }
-function getUser(sessionId){
-  return sessionIdToUserMap.get(sessionId);
+function getUser(token){
+  if(!token) return null;
+  try {
+    return jwt.verify(token,secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports={
